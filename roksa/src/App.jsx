c138c9afd3a633b9dd88e0d2d612036392506a97
Роксолана <roksolana.yadzhak.oi.2023@gmail.com>
2025-05-24 14:00:23 +0300

    Оновлено структуру: нові компоненти, видалені зайві файли

---
+++ roksa/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import Hero from "./components/Hero";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
